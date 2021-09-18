8415f05e6839f8071ac60e5c7f93aef3a8b0b5dd
Kevin Li <likevintw@gmail.com>
2021-09-18 15:29:09 +0800

    first commit

--- test_openpyxl.py
+++ test_openpyxl.py
@@ -9,7 +9,7 @@ import openpyxl
 class TestFolderPathHandler(unittest.TestCase):
     @unittest.skip("Pass")
     def test_show_sheet_title(self):
-        workbook = openpyxl.load_workbook("demonstration.xlsx")
+        workbook = openpyxl.load_workbook("test.xlsx")
         for sheet in workbook:
             print(sheet.title)  # get a string
 
